27de7acda0c1ebde89b175970a308d118689625a
Victor Ryan <106771198+VictorRyan3612@users.noreply.github.com>
2023-05-28 13:52:45 -0300

    Quick sort parte 1

---
+++ quick.c
@@ -0,0 +1,14 @@
+
+
+void partition (int *v[],int s, int e){
+    int d = s;
+    for (int i =0; i< (e-1); i++){
+        if (v[i] < v[e]){
+            swap(v[i], v[d+1]);
+            d += 1;
+        }
+        swap (v[d+1], v[i]);
+
+    }
+    return d;
+}
