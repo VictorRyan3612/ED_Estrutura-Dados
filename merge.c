3cf642050fe1721583ef42a41be4ac92b6a149e1
Victor Ryan <106771198+VictorRyan3612@users.noreply.github.com>
2023-05-28 12:13:46 -0300

    Merge part 3

--- merge.c
+++ merge.c
@@ -1,5 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
-
+#define CLOCK_MONOTONIC 1
 void merge (int v[], int s, int m, int e){
     int p = s;
     int q = m+1;
@@ -30,6 +33,29 @@ void merge_sort (v,s,e){
 
 int main(int argc, char const *argv[])
 {
-    
+    struct timespec a, b;
+    unsigned int t, n;
+    int i, *v;
+
+    n = atoi(argv[1]);
+    v = (int *) malloc(n * sizeof(int));
+
+
+    srand(time(NULL));
+    for (i = 0; i < n; i++)
+        v[i] = rand();
+
+
+
+
+    clock_gettime(CLOCK_MONOTONIC, &b);
+    merge_sort(v, n);
+    clock_gettime(CLOCK_MONOTONIC, &a);
+
+    t = (a.tv_sec * 1e9 + a.tv_nsec) - (b.tv_sec * 1e9 + b.tv_nsec);
+
+    printf("%u\n", t);
+
+    free(v);
     return 0;
 }
