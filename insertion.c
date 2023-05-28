3e21031c69571db125694770f31dc4a9bb3110b2
Victor Ryan <106771198+VictorRyan3612@users.noreply.github.com>
2023-05-28 15:12:48 -0300

    Insertion part 3

--- insertion.c
+++ insertion.c
@@ -17,7 +17,7 @@ void insertion(int v[], int n){
     for (int i = 0; i<n;i++){
         int j = i;
         while ((j>0) && (v[j] < v[j-1])){
-            swap (v[j], v[j-1]);
+            swap (&v[j], &v[j-1]);
             j = j-1;
         }
     }
