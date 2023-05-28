f46db153e464d9b793ba27258853b0e88e5fcb10
Victor Ryan <106771198+VictorRyan3612@users.noreply.github.com>
2023-05-28 14:31:25 -0300

    Quick 5

--- quick.c
+++ quick.c
@@ -1,9 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+// #include <stdlib.h>
+
+#define CLOCK_MONOTONIC 1
+
+
+
+
 void swap(int *a, int *b)
 {
-    int m;
-    m = *a;
+    int aux;
+    aux = *a;
     *a = *b;
-    *b = m;
+    *b = aux;
 }
 
 int partition(int *v, int s, int e)
@@ -32,4 +41,42 @@ void quick_sort(int *v, int s, int e)
         quick_sort(v, s, p - 1);
         quick_sort(v, p + 1, e);
     }
+}
+
+criarvetor(int v[], int n)
+{
+    // Preenche o vetor com valores aleatórios em ordem crescente
+    srand(time(NULL));
+    v[0] = rand();
+    for (int i = 1; i < n; i++){
+        v[i] = v[i - 1] + rand();
+    }
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    struct timespec a, b;
+    unsigned int t, n;
+    int i, *v;
+
+    n = atoi(argv[1]);
+    v = (int *) malloc(n * sizeof(int));
+
+    criarvetor(v, n);
+
+
+    clock_gettime(CLOCK_MONOTONIC, &b);
+    quick_sort(v, 0, n-1);
+    clock_gettime(CLOCK_MONOTONIC, &a);
+
+    t = (a.tv_sec * 1e9 + a.tv_nsec) - (b.tv_sec * 1e9 + b.tv_nsec);
+
+    printf("%u\n", t);
+
+    free(v);
+
+    return 0;
 }
