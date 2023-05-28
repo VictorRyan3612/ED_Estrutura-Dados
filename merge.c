dce1ba366287d4660412ee1c5adf6fcc2194d1e8
Victor Ryan <106771198+VictorRyan3612@users.noreply.github.com>
2023-05-28 12:01:35 -0300

    Merge part 2

--- merge.c
+++ merge.c
@@ -3,7 +3,7 @@
 void merge (int v[], int s, int m, int e){
     int p = s;
     int q = m+1;
-    for (int i; i < (e-s+1); i++){
+    for (int i = 0; i < (e-s+1); i++){
         if ((q>e) || ((p<=m) && (v[p] < v[q]))) {
             v[i] = v[p];
             p = p+1;
@@ -13,12 +13,20 @@ void merge (int v[], int s, int m, int e){
             q= q+1;
         }
     }
-    for (int i; i< (e-s+1); i++){
-        v[s+i-1] = v[i];
+    for (int i = 0; i< (e-s+1); i++){
+        v[s+i] = v[i];
     }
 }
 
 
+void merge_sort (v,s,e){
+    if (s<e){
+        int m =((s+e)/2);
+        merge_sort(v,s,m);
+        merge_sort(v,m+1,e);
+        merge(v,s,m,e);
+    }
+}
 
 int main(int argc, char const *argv[])
 {
