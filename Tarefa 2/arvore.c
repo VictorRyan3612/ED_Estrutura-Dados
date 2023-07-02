210eab1e0d03390496f1c4876f1bd05bd82a28eb
Victor <vitorsilva3612@gmail.com>
2023-07-02 16:20:10 -0300

    inserção aleatoria
    Arvore desbalanceada parte 3

--- Tarefa 2/arvore.c
+++ Tarefa 2/arvore.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Estrutura de um nó da árvore
 typedef struct No {
@@ -40,17 +41,23 @@ void percorrer(No* root) {
     }
 }
 
+// Função para inserir valores aleatórios na árvore
+No* inserirAleatorio(No* root, int quantidade, int valorMaximo) {
+    srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
+
+    for (int i = 0; i < quantidade; i++) {
+        int valor = rand() % valorMaximo + 1; // Gera um número aleatório entre 1 e valorMaximo
+        root = inserir(root, valor);
+    }
+
+    return root;
+}
+
 int main() {
     No* root = NULL;
 
-    // Exemplo de inserção de valores na árvore
-    root = inserir(root, 50);
-    root = inserir(root, 30);
-    root = inserir(root, 70);
-    root = inserir(root, 20);
-    root = inserir(root, 40);
-    root = inserir(root, 60);
-    root = inserir(root, 80);
+    // Inserir 10 valores aleatórios na árvore, com valores entre 1 e 100
+    root = inserirAleatorio(root, 10, 100);
 
     // Exemplo de percorrimento da árvore em ordem
     printf("Árvore em ordem: ");
